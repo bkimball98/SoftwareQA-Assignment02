3733394fdb8e8e3947e0e35a3060e7f09ee432c6
Braedon Kimball <48100828+bkimball98@users.noreply.github.com>
2021-02-25 17:17:17 -0600

    Update cmdapp.py

--- cmdapp.py
+++ cmdapp.py
@@ -1,1 +1,40 @@
-print("This is a test")
+#Written by Braedon Kimball (bmk228) for Software Quality Assurance
+def BMI():
+	print("BMI goes here.")
+
+
+def retirement():
+	print("Retirement goes here.")
+
+
+
+
+
+
+def main():
+
+	print("Welcome to the application! Options below: \n")
+	x = 0
+	while(x==0):
+		
+		print("1: Calculate BMI \n2: Calculate Retirement\n3: Exit the program\n")
+		var = input()
+		
+		if var == '1':
+			BMI()
+			
+		
+		elif var == '2':
+			retirement()
+		
+		elif(var == '3'):
+			break
+		
+		else:
+			print("Please input a valid option, either 1 or 2.\n")
+
+	return
+
+
+main()
+
